92b86ba695f08be95f269a2afbc5e1de0f4454d4
Mahesh Nimkar <maheshnimkar@outlook.com>
2023-09-29 21:46:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import HeroSection from "./components/HeroSection/HeroSection";
+import MainBodyTopText from "./components/MainBodyTopText/MainBodyTopText";
+import Choose from "./components/Choose/Choose";
+import GridSection from "./components/GridSection/GridSection";
+import PeopleBox from "./components/PeopleBox/PeopleBox";
+import Members from "./components/Members/Members";
+import Faq from "./components/FAQ/Faq";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fullBody">
+      <div className="TopTwoSection">
+        <NavBar />
+        <HeroSection />
+      </div>
+      <MainBodyTopText />
+      <Choose />
+      <GridSection />
+      <PeopleBox />
+      <Members />
+      <Faq />
+      <Subscribe />
+      <Footer />
     </div>
   );
 }
